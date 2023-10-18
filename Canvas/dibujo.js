54e754246789dd2661458bab108fd49eadcd79cc
JesusGarciaShakur <jesusrguez970@gmail.com>
2023-10-18 12:59:31 -0600

    06 Canvas con HTML

---
+++ Canvas/dibujo.js
@@ -0,0 +1,123 @@
+const lienzo = document.querySelector('#lienzo');
+
+const ctx = lienzo.getContext('2d');
+ctx.fillStyle = 'black'
+//primera linea de arriba
+ctx.fillRect(170, 50, 30, 30);
+ctx.fillRect(200, 50, 30, 30);
+ctx.fillRect(230, 50, 30, 30);
+ctx.fillRect(260, 50, 30, 30);
+ctx.fillRect(290, 50, 30, 30);
+ctx.fillRect(320, 50, 30, 30);
+
+
+ctx.fillRect(140, 80, 30, 30);
+ctx.fillRect(110, 80, 30, 30);
+ctx.fillRect(80, 110, 30, 30);
+ctx.fillRect(80, 140, 30, 30);
+
+
+ctx.fillRect(50, 170, 30, 30);
+ctx.fillRect(50, 200, 30, 30);
+ctx.fillRect(50, 230, 30, 30);
+ctx.fillRect(50, 260, 30, 30);
+ctx.fillRect(50, 290, 30, 30);
+ctx.fillRect(50, 320, 30, 30);
+
+ctx.fillRect(80, 350, 30, 30);
+ctx.fillRect(110, 350, 30, 30);
+ctx.fillRect(110, 380, 30, 30);
+
+//conexión debajo de ojos
+ctx.fillRect(170, 320, 30, 30);
+ctx.fillRect(200, 320, 30, 30);
+ctx.fillRect(230, 320, 30, 30);
+ctx.fillRect(260, 320, 30, 30);
+ctx.fillRect(230, 350, 30, 30);//ojo izquierdo
+ctx.fillRect(230, 380, 30, 30);//ojo izquierdo complemento
+ctx.fillRect(330, 350, 30, 30);//ojo derecho
+ctx.fillRect(330, 380, 30, 30);//ojo derecho complemento
+ctx.fillRect(290, 320, 30, 30);
+ctx.fillRect(320, 320, 30, 30);
+ctx.fillRect(350, 320, 30, 30);
+ctx.fillRect(380, 320, 30, 30);
+
+
+ctx.fillRect(110, 410, 30, 30);
+
+
+ctx.fillRect(170, 470, 30, 30); //linea de abajo
+ctx.fillRect(200, 470, 30, 30); //linea de abajo
+ctx.fillRect(230, 470, 30, 30); //linea de abajo
+ctx.fillRect(260, 470, 30, 30); //linea de abajo
+ctx.fillRect(290, 470, 30, 30); //linea de abajo
+ctx.fillRect(320, 470, 30, 30); //linea de abajo
+ctx.fillRect(350, 470, 30, 30); //linea de abajo
+ctx.fillRect(380, 470, 30, 30); //linea de abajo
+
+
+ctx.fillRect(140, 440, 30, 30);
+ctx.fillRect(140, 350, 30, 30);
+
+//otra parte del contorno
+ctx.fillRect(410, 440, 30, 30);
+ctx.fillRect(440, 410, 30, 30);
+ctx.fillRect(440, 380, 30, 30);
+ctx.fillRect(440, 350, 30, 30);
+
+ctx.fillRect(470, 350, 30, 30);
+ctx.fillRect(410, 350, 30, 30);
+
+
+ctx.fillRect(500, 320, 30, 30);
+ctx.fillRect(500, 290, 30, 30);
+ctx.fillRect(500, 260, 30, 30);
+ctx.fillRect(500, 230, 30, 30);
+ctx.fillRect(500, 200, 30, 30);
+ctx.fillRect(500, 170, 30, 30);
+
+ctx.fillRect(470, 140, 30, 30);
+ctx.fillRect(470, 110, 30, 30);
+
+ctx.fillRect(440, 80, 30, 30);
+ctx.fillRect(410, 80, 30, 30);
+
+ctx.fillRect(380, 50, 30, 30);
+ctx.fillRect(350, 50, 30, 30);
+
+
+
+//palabra RIP
+ctx.fillStyle = 'purple'
+//letra r
+ctx.fillRect(140, 140, 30, 30);
+ctx.fillRect(140, 170, 30, 30);
+ctx.fillRect(140, 200, 30, 30);
+ctx.fillRect(140, 230, 30, 30);
+ctx.fillRect(140, 260, 30, 30);
+ctx.fillRect(170, 140, 30, 30);
+ctx.fillRect(200, 140, 30, 30);
+ctx.fillRect(230, 170, 30, 30);
+ctx.fillRect(230, 230, 30, 30);
+ctx.fillRect(230, 260, 30, 30);
+ctx.fillRect(200, 200, 30, 30);
+ctx.fillRect(170, 200, 30, 30);
+
+//letra i
+ctx.fillRect(290, 140, 30, 30);
+ctx.fillRect(290, 170, 30, 30);
+ctx.fillRect(290, 200, 30, 30);
+ctx.fillRect(290, 230, 30, 30);
+ctx.fillRect(290, 260, 30, 30);
+
+//letra p
+ctx.fillRect(350, 140, 30, 30);
+ctx.fillRect(380, 140, 30, 30);
+ctx.fillRect(410, 140, 30, 30);
+ctx.fillRect(410, 170, 30, 30);
+ctx.fillRect(410, 200, 30, 30);
+ctx.fillRect(380, 200, 30, 30);
+ctx.fillRect(350, 170, 30, 30);
+ctx.fillRect(350, 200, 30, 30);
+ctx.fillRect(350, 230, 30, 30);
+ctx.fillRect(350, 260, 30, 30);
